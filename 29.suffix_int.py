9841e96d0a2c51807e0d51eb4e4f517f4b124983
vildan-valeev <58739557+vildan-valeev@users.noreply.github.com>
2020-09-05 22:26:17 +0500

    args, kwargs

--- 29.suffix_int.py
+++ 29.suffix_int.py
@@ -8,14 +8,14 @@
 
 k = float(input())
 
-if 1 <= k <= 99 and k.is_integer():
+if 1 <= k and k.is_integer():
     last = k % 10
     if int(last) == 1 and not k == 11:
-        print(f'{int(k)} рубль')
+        print(f'{int(k)} поездка')
     elif 1 < last < 5 and not 11 < k < 15:
-        print(f'{int(k)} рубля')
+        print(f'{int(k)} поездки')
     else:
-        print(f'{int(k)} рублей')
+        print(f'{int(k)} поездок')
 else:
     print('ошибка')
 
