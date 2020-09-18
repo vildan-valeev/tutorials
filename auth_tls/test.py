baabb3f77dc159a7fddfad4352148738f1e36cdf
vildan-valeev <58739557+vildan-valeev@users.noreply.github.com>
2020-09-18 14:14:20 +0500

    urllib request

--- auth_tls/test.py
+++ auth_tls/test.py
@@ -1,49 +1,33 @@
 import json
-from urllib import request
-# import requests
+import urllib
+
+import requests
 from urllib.parse import urlencode
-from urllib.request import urlopen, Request, URLopener
+from urllib.request import urlopen, Request, URLopener, HTTPSHandler
+import ssl
+
 
 cert = 'client03test.crt'
 key = 'client03test.key'
 url = "https://slb.medv.ru/api/v2/"
-headers = {
-    'content-type': 'application/json',
-}
-values = {
-    "method": "auth.check",
-    "params": [],
-    "jsonrpc": "2.0",
-    "id": 1,
-}
+headers = {'content-type': 'application/json'}
+values = {"method": "auth.check", "params": [], "jsonrpc": "2.0", "id": 1}
 
 
-# def main():
-#     url = "https://slb.medv.ru/api/v2/"
-#     headers = {
-#         'content-type': 'application/json',
-#     }
-#     payload = {
-#         "method": "auth.check",
-#         "params": [],
-#         "jsonrpc": "2.0",
-#         "id": 1,
-#     }
-#
-#     # response = requests.post(url, cert=(cert, key), verify=True, headers=headers, data=json.dumps(payload))
-#     response = requests.post(url, cert=(CERTIFICATE, KEY), verify=True, headers=headers, data=json.dumps(payload))
-#     print(response.json())
-
+def main():
+    """requests lib"""
+    response = requests.post(url, cert=(cert, key), verify=True, headers=headers, data=json.dumps(values))
+    print(response.json())
 
 def main2():
-    # res = request.Request(url=url, headers=headers, )
-    data = urlencode(values).encode()
-    URLopener(key_file=key, cert_file=cert)
-    print(urlopen(Request(url=url, data=data, headers=headers, ), timeout=60).read().decode())
+    """urllib"""
+    data = json.dumps(values).encode('utf-8')
 
-    # response = requests.post(url, cert=(cert, key), verify=True, headers=headers, data=json.dumps(payload))
-    # response = requests.post(url, cert=(CERTIFICATE, KEY), verify=True, headers=headers, data=json.dumps(payload))
-    # print(response.json())
+    context = ssl.create_default_context()
+    context.load_cert_chain(certfile=cert, keyfile=key)
+    opener = urllib.request.build_opener(urllib.request.HTTPSHandler(context=context))
+    response = opener.open(Request(url=url, data=data, headers=headers, )).read().decode()
+    print(response)
 
 
 if __name__ == "__main__":
