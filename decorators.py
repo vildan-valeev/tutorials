8c7e3a4adeec4aec3774ba5ec30563530fbac858
vildan-valeev <58739557+vildan-valeev@users.noreply.github.com>
2020-09-20 23:33:26 +0500

    stepik

--- decorators.py
+++ decorators.py
@@ -1,4 +1,49 @@
+method = 'auth.check'
+code_response = 200
 
 
-def one():
-    pass
+class Service:
+    def __init__(self, request):
+        self.request = request
+
+
+    def check(self):
+        if self.request =
+
+
+
+############################
+s = Service
+
+
+@s.check
+def main(request):
+    print("Ок, добро )))")
+
+
+if __name__ == '__main__':
+    main('пароль')
+
+
+
+
+
+
+########################################
+
+# def decorator_function(func):
+#     def wrapper():
+#         print('Функция-обёртка!')
+#         print('Оборачиваемая функция: {}'.format(func))
+#         print('Выполняем обёрнутую функцию...')
+#         func()
+#         print('Выходим из обёртки')
+#
+#     return wrapper
+#
+#
+# @decorator_function
+# def hello_world():
+#     print('Hello world!')
+#
+
