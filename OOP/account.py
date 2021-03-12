07b15a881d2f1bb591d752c4444a2b4b4b9d25a0
vildan-valeev <vildan.maydanovich.valeev@gmail.com>
2021-03-12 09:08:05 +0500

    name mangling

--- OOP/account.py
+++ OOP/account.py
@@ -10,34 +10,33 @@ RED = '\033[1;31m'
 class Account:
     def __init__(self, name, balance):
         self.name = name
-        self.balance = balance
-        self.show_balance()
-        self.history = []
+        self.__balance = balance
+        self._history = []
 
     @staticmethod
     def _get_current_time():
         return pytz.utc.localize(datetime.utcnow())
 
     def deposit(self, amount):
-        self.balance += amount
+        self.__balance += amount
         self.show_balance()
-        self.history.append([amount, self._get_current_time()])
+        self._history.append([amount, self._get_current_time()])
 
     def withdraw(self, amount):
-        if self.balance > amount:
-            self.balance -= amount
+        if self.__balance > amount:
+            self.__balance -= amount
             print(f'You spent ${amount}')
             self.show_balance()
-            self.history.append([-amount, self._get_current_time()])
+            self._history.append([-amount, self._get_current_time()])
         else:
             print('Not Enough money')
             self.show_balance()
 
     def show_balance(self):
-        print(f'Balance: {self.balance}')
+        print(f'Balance: {self.__balance}')
 
     def show_history(self):
-        for amount, date in self.history:
+        for amount, date in self._history:
             if amount > 0:
                 transaction = 'deposited'
                 color = GREEN
@@ -47,10 +46,5 @@ class Account:
             print(f'{color} {amount} {WHITE} {transaction} on {date.astimezone()}')
 
 
-a = Account('oleg', 0)
-# print(a.show_balance())
-a.deposit(100)
-# print(a.show_balance())
-a.withdraw(50)
-# print(a.show_balance())
-a.show_history()
+a = Account('oleg', 1000)
+a.show_balance()
